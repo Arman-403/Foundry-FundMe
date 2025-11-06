191d37e600c831a7f974e011d3d98576801036d1
{username}Arman-403 <armans04@Cypher.localdomain>
2025-11-06 15:57:46 +0530

    feat: integrate PriceConverter library into FundMe contract

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -23,25 +23,27 @@
 
 pragma solidity ^0.8.24;
 
-import {AggregatorV3Interface} from
-    "chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 /*//////////////////////////////////////////////////////////////
                                 ERRORS
 //////////////////////////////////////////////////////////////*/
+
 error FundMe__YOU_ARE_NOT_THE_OWNER();
 error FundMe__WITHDRAW_FAILED();
 
 contract FundMe {
+    using PriceConverter for uint256;
     /*//////////////////////////////////////////////////////////////
-                            STATE VARIABLES
+                              STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
+
     uint256 private constant MIN_USD = 5e18;
 
     address private immutable i_owner;
 
-    AggregatorV3Interface private s_priceFeed;
     address[] private s_funders;
+    AggregatorV3Interface private s_priceFeed;
     mapping(address => uint256) private s_funderToAmountFunded;
 
     modifier OwnerOnly() {
@@ -64,35 +66,33 @@ contract FundMe {
         fund();
     }
 
-    function getPrice() internal view returns (uint256) {
-        (, int256 price,,,) = s_priceFeed.latestRoundData();
-        return (uint256(price * 1e10));
-    }
+    // function getPrice() internal view returns (uint256) {
+    //     (, int256 price,,,) = s_priceFeed.latestRoundData();
+    //     return (uint256(price * 1e10));
+    // }
 
-    function getConversion(uint256 amountInWei) internal view returns (uint256) {
-        uint256 price = getPrice();
-        uint256 finaUsd = ((amountInWei) * (price)) / 1e18;
+    // function getConversion(uint256 amountInWei) internal view returns (uint256) {
+    //     uint256 price = getPrice();
+    //     uint256 finaUsd = ((amountInWei) * (price)) / 1e18;
 
-        return finaUsd;
-    }
+    //     return finaUsd;
+    // }
 
     function fund() public payable {
-        require(getConversion(msg.value) >= MIN_USD, "Send More Eth");
+        require(msg.value.getConversionRate(s_priceFeed) >= MIN_USD, "Send More Eth");
         s_funders.push(msg.sender);
         s_funderToAmountFunded[msg.sender] += msg.value;
     }
 
     function withdraw() external OwnerOnly {
         // this is good way but we can save more gas
-
         // uint256 length = s_funders.length;
-
         // for (uint256 i = 0; i < length; i++) {
         //     address funder = s_funders[i];
         //     s_funderToAmountFunded[funder] = 0;
         // }
 
-        // gas efficient version - copying s_funders array in memory its cheaper to hanlde ( remeber here we got MLOAD MSTORE that need ~3 gas)
+        // gas efficient version
         address[] memory funders = s_funders;
         for (uint256 i = 0; i < funders.length; i++) {
             s_funderToAmountFunded[funders[i]] = 0;
@@ -130,7 +130,7 @@ contract FundMe {
         return s_funders[index];
     }
 
-    function getFundersArrayLength() public view returns (uint256) {
-        return s_funders.length;
-    }
+    // function getFundersArrayLength() public view returns (uint256) {
+    //     return s_funders.length;
+    // }
 }
